d6c3d00c6e5077e83b9634d60c58ef085f93d989
Ricardo Rivera <hello@webbrah.com>
2023-04-13 22:56:03 -0600

    Hero

--- little-lemon-restaurant/src/App.js
+++ little-lemon-restaurant/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero.jsx';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Hero />
+    </>
   );
 }
 
---
+++ little-lemon-restaurant/src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <>
+      <section>
+        <h1>Name</h1>
+        <p>Description</p>
+        <button>Reserve a table</button>
+      </section>
+    </>
+  );
+}
