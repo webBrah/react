5006aa900d48c4b01e434a7a9a1546a28ff434da
Ricardo Rivera <hello@webbrah.com>
2023-04-18 22:46:56 -0600

    Specials Component

--- little-lemon-restaurant/src/App.js
+++ little-lemon-restaurant/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero.jsx';
 import Hompage from './components/Homepage/Homepage';
+import Specials from './components/Specials/Specials';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Hompage />
+      <Specials />
     </>
   );
 }
---
+++ little-lemon-restaurant/src/components/Specials/Specials.jsx
@@ -0,0 +1,9 @@
+export default function Specials() {
+  return (
+    <>
+      <p>This is the Specials component</p>
+    </>
+  );
+}
+
+// need to create CSS file
