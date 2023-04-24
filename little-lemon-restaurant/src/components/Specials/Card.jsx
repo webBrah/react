0843e401d20a07fd9984e64b886176b0ea9b8301
Ricardo Rivera <hello@webbrah.com>
2023-04-23 21:26:50 -0600

    Updated Specials

--- little-lemon-restaurant/src/components/Specials/Card.jsx
+++ little-lemon-restaurant/src/components/Specials/Card.jsx
@@ -4,6 +4,9 @@ function Card(props) {
   return (
     <div className="card">
       <div className="container">
+        <div className="image-wrapper">
+          <img src={props.src} alt={props.alt} />
+        </div>
         <h2>{props.h2}</h2>
         <h3>{props.h3}</h3>
         <p>{props.p}</p>
@@ -14,3 +17,4 @@ function Card(props) {
 }
 
 export default Card;
+
--- little-lemon-restaurant/src/components/Specials/Specials.jsx
+++ little-lemon-restaurant/src/components/Specials/Specials.jsx
@@ -1,5 +1,8 @@
 import Card from './Card';
 import './Specials.css';
+import GreekSalad from '../../assets/food/greek-salad.webp';
+import Bruschetta from '../../assets/food/bruschetta.jpeg';
+import LemonDessert from '../../assets/food/lemon-dessert.webp';
 
 const menuItems = [
   {
@@ -8,6 +11,7 @@ const menuItems = [
     price: '$12.99',
     description:
       'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutions.',
+    image: GreekSalad
   },
   {
     id: '2',
@@ -15,6 +19,7 @@ const menuItems = [
     price: '$5.99',
     description:
       'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.',
+    image: Bruschetta
   },
   {
     id: '3',
@@ -22,6 +27,7 @@ const menuItems = [
     price: '$5.00',
     description:
       "This comes stright from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+      image: LemonDessert
   },
 ];
 
@@ -42,11 +48,13 @@ export default function Specials(props) {
           <div className="card">
             {menuItems.map((item) => (
               <Card
-                key={item.id}
-                h2={item.title}
-                h3={item.price}
-                p={item.description}
-              />
+              key={item.id}
+              src={item.image}
+              alt={item.title}
+              h2={item.title}
+              h3={item.price}
+              p={item.description}
+            />
             ))}
           </div>
         </div>
