c5878fec886cb94b007ec24778f7166857775586
Ricardo Rivera <hello@webbrah.com>
2023-04-24 21:18:26 -0600

    Updated Card Styles

--- little-lemon-restaurant/src/components/Specials/Card.jsx
+++ little-lemon-restaurant/src/components/Specials/Card.jsx
@@ -7,14 +7,15 @@ function Card(props) {
         <div className="image-wrapper">
           <img src={props.src} alt={props.alt} />
         </div>
-        <h2>{props.h2}</h2>
-        <h3>{props.h3}</h3>
-        <p>{props.p}</p>
-        <button>Order for Delivery</button>
+        <div className="card-content">
+          <h2>{props.h2}</h2>
+          <h3>{props.h3}</h3>
+          <p>{props.p}</p>
+          <button>Order for Delivery</button>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Card;
-
