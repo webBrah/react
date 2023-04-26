d148dd89604f5306363487300d11dcc3483fb21f
Ricardo Rivera <hello@webbrah.com>
2023-04-25 22:20:34 -0600

    Page Updates

--- little-lemon-restaurant/src/components/CustomersSay/Card.jsx
+++ little-lemon-restaurant/src/components/CustomersSay/Card.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
 import './Card.css';
+import starImage from '../../assets/star.png';
 
 function Card(props) {
   return (
     <div className="card">
       <div className="container">
+        <div className="image-wrapper">
+          <img src={starImage} alt={props.name}/>
+        </div>
         <h3>{props.h3}</h3>
         <p>{props.p}</p>
-     </div>
+      </div>
     </div>
   );
 }
--- little-lemon-restaurant/src/components/CustomersSay/CustomersSay.jsx
+++ little-lemon-restaurant/src/components/CustomersSay/CustomersSay.jsx
@@ -4,24 +4,28 @@ import './CustomersSay.css';
 const testimonials = [
   {
     id: '1',
+    image: '../../assets/star.png',
     name: 'John Doe',
     review:
       'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutions.',
   },
   {
     id: '2',
+    image: '../../assets/star.png',
     name: 'Jane Smith',
     review:
       'The bruschetta was absolutely delicious, with the perfect amount of garlic and olive oil.',
   },
   {
     id: '3',
+    image: '../../assets/star.png',
     name: 'Mike Johnson',
     review:
       "I couldn't resist trying the lemon dessert, and it was just as authentic as promised. Highly recommended!",
   },
   {
     id: '4',
+    image: '../../assets/star.png',
     name: 'Sarah Lee',
     review:
       'The Greek salad was a delight to my taste buds. The feta cheese was definitely the star of the dish!',
@@ -34,7 +38,12 @@ export default function CustomersSay() {
       <h2 className='title-testimonials'>Testimonials</h2>
       <div className="card-wrapper">
         {testimonials.map((item) => (
-          <Card key={item.id} h3={item.name} p={item.review} />
+          <Card
+            key={item.id}
+            h3={item.name}
+            p={item.review}
+            image={item.image}
+          />
         ))}
       </div>
     </section>
