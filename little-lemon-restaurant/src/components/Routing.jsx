4256048ed6051c41fa2bf6aa6325dd554c9a3671
Ricardo Rivera <hello@webbrah.com>
2023-04-25 20:50:10 -0600

    Pages and Routing Update

--- little-lemon-restaurant/src/components/Routing.jsx
+++ little-lemon-restaurant/src/components/Routing.jsx
@@ -4,24 +4,19 @@ import About from './Pages/About';
 import Menu from './Pages/Menu';
 import Reservations from './Pages/Reservations';
 import Order from './Pages/Order';
-import Login from "./Pages/Login";
-import Confirmation from "./Pages/Confirmation";
+import Login from './Pages/Login';
+import Confirmation from './Pages/Confirmation';
 
 export default function Routing() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-
       <Route path="/about" element={<About />} />
-
       <Route path="/menu" element={<Menu />} />
-
       <Route path="/reservations" element={<Reservations />} />
-
       <Route path="/order" element={<Order />} />
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/confirmation" element={<Confirmation />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/confirmation" element={<Confirmation />} />
     </Routes>
   );
 }
