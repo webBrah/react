8fb8db824753c3216ace607a1450f782035ee8ad
Ricardo Rivera <hello@webbrah.com>
2023-04-29 20:44:44 -0600

    Update Styles

--- little-lemon-restaurant/src/components/Chicago/Chicago.jsx
+++ little-lemon-restaurant/src/components/Chicago/Chicago.jsx
@@ -2,12 +2,19 @@ import './Chicago.css';
 
 export default function Chicago() {
   return (
-    <section className='chicago-wrapper'>
+    <section className="chicago-wrapper">
       <div className="chicago">
         <div className="chicago-text">
           <h2>Little Lemon</h2>
           <h3>Chicago</h3>
-          <p className="subsection">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
+          <p className="subsection">
+            Little Lemon opened in 1995 by two Italian brothers, Adrian and
+            Mario. Despite the city's diversity, the two brothers recognized the
+            lack of Mediterranean cuisine in Chicago, and were inspired to bring
+            the flavors of their hometown in Italy to the people of Chicago. The
+            two brothers continue to oversee the Little Lemon restaurant, nearly
+            thirty years later.
+          </p>
         </div>
         <div className="chicago-images">
           <img
@@ -16,7 +23,7 @@ export default function Chicago() {
             className="chicago-image image-adrian"
           ></img>
           <img
-            src={require('../../assets/food/food3.jpeg')}
+            src={require('../../assets/food/food2.jpeg')}
             alt="Little Lemon restaurant cuisine"
             className="chicago-image image-nick"
           ></img>
@@ -25,4 +32,3 @@ export default function Chicago() {
     </section>
   );
 }
-
