35b6c30db8cdfa79355a69fd4d49bf495e1186c2
Ricardo Rivera <hello@webbrah.com>
2023-04-29 19:48:28 -0600

    Testimonials Update

--- little-lemon-restaurant/src/components/CustomersSay/CustomersSay.jsx
+++ little-lemon-restaurant/src/components/CustomersSay/CustomersSay.jsx
@@ -1,24 +1,27 @@
 import CardTestimonials from './CardTestimonials';
 import './CustomersSay.css';
+import testimonial1 from '../../assets/testimonials-1.jpg'; // Import the image files
+import testimonial2 from '../../assets/testimonials-2.jpg';
+import testimonial3 from '../../assets/testimonials-3.jpg';
 
 const testimonials = [
   {
     id: '1',
-    image: '../../assets/testimonials-1.jpg',
+    image: testimonial1, // Use the imported images
     name: 'John Doe',
     review:
       'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutions.',
   },
   {
     id: '2',
-    image: '../../assets/testimonials-2.jpg',
+    image: testimonial2,
     name: 'Jane Smith',
     review:
       'The bruschetta was absolutely delicious, with the perfect amount of garlic and olive oil.',
   },
   {
     id: '3',
-    image: '../../assets/testimonials-3.jpg',
+    image: testimonial3,
     name: 'Mike Johnson',
     review:
       "I couldn't resist trying the lemon dessert, and it was just as authentic as promised. Highly recommended!",
