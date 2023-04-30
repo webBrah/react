27d127673dd86b7d5b3bb40e11b69dae81ed4ff2
Ricardo Rivera <hello@webbrah.com>
2023-04-29 18:02:11 -0600

    Added link

--- little-lemon-restaurant/src/components/Specials/Card.jsx
+++ little-lemon-restaurant/src/components/Specials/Card.jsx
@@ -1,4 +1,5 @@
 import './Card.css';
+import { Link } from 'react-router-dom';
 
 function Card(props) {
   return (
@@ -10,10 +11,12 @@ function Card(props) {
         <div className="card-content">
           <div className="title-row">
             <h2>{props.h2}</h2>
-            <h3 class="price">{props.h3}</h3>
+            <h3 className="price">{props.h3}</h3>
           </div>
           <p>{props.p}</p>
-          <button>Order for Delivery</button>
+          <Link to="/login">
+            <button>Order for Delivery</button>
+          </Link>
         </div>
       </div>
     </div>
