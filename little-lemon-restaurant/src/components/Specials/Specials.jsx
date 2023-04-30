2907e59a01c58dc3f5fa30e887cf606c62ea25f6
Ricardo Rivera <hello@webbrah.com>
2023-04-29 18:47:21 -0600

    Style Updates

--- little-lemon-restaurant/src/components/Specials/Specials.jsx
+++ little-lemon-restaurant/src/components/Specials/Specials.jsx
@@ -3,6 +3,7 @@ import './Specials.css';
 import GreekSalad from '../../assets/food/greek-salad.webp';
 import Bruschetta from '../../assets/food/bruschetta.jpeg';
 import LemonDessert from '../../assets/food/lemon-dessert.webp';
+import { Link } from 'react-router-dom';
 
 const menuItems = [
   {
@@ -11,7 +12,7 @@ const menuItems = [
     price: '$12.99',
     description:
       'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutions.',
-    image: GreekSalad
+    image: GreekSalad,
   },
   {
     id: '2',
@@ -19,7 +20,7 @@ const menuItems = [
     price: '$5.99',
     description:
       'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.',
-    image: Bruschetta
+    image: Bruschetta,
   },
   {
     id: '3',
@@ -27,7 +28,7 @@ const menuItems = [
     price: '$5.00',
     description:
       "This comes stright from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
-      image: LemonDessert
+    image: LemonDessert,
   },
 ];
 
@@ -40,14 +41,15 @@ export default function Specials(props) {
             <h2>This week's specials!</h2>
           </div>
           <div className="specials-btn">
-            <button className="btn">Online Menu</button>
+            <Link to="/menu">
+              <button className="btn">Online Menu</button>
+            </Link>
           </div>
         </div>
 
-        <div className="card-wrapper">
-          <div className="card">
-            {menuItems.map((item) => (
-              <Card
+        <div className="cards-wrapper">
+          {menuItems.map((item) => (
+            <Card
               key={item.id}
               src={item.image}
               alt={item.title}
@@ -55,8 +57,7 @@ export default function Specials(props) {
               h3={item.price}
               p={item.description}
             />
-            ))}
-          </div>
+          ))}
         </div>
       </section>
     </div>
